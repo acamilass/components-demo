58c04ce2ff8c73a7ee68e0816e4ae6413f99571c
Ana Camila Santos Sousa <acamilass@MacBook-Air-de-Ana.local>
2021-06-01 22:11:58 -0300

    Criando primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardCarouselComponent } from './components/card-carousel/card-carousel.component';
+import { SimpleCarouselComponent } from './components/simple-carousel/simple-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardCarouselComponent,
+    SimpleCarouselComponent
   ],
   imports: [
     BrowserModule,
